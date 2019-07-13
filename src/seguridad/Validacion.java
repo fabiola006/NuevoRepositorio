76d7a874bc9dbd98120f3ffb086cf7eda3c5cdc3
LAB-USR-SJL275-B0202 <LAB-USR-SJL275-B0202@grupoutp.net>
2019-07-13 16:54:41 -0500

    pray for Alonso

--- src/seguridad/Validacion.java
+++ src/seguridad/Validacion.java
@@ -5,6 +5,7 @@ public class Validacion {
         String msg = "";
         msg = "esta correcto el usuario "
                 + "Jhampierr estuvo aqui"
+                + "Pray for Alonso"
                 + "seleccionado, no olvide cambiar "
                 + "su contrasena en menos "
                 + "de 90 dias";;
